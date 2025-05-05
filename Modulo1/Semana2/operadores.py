445ac77d3a4dd0d8697b81286b928b6887c4071c
Jess Cruz <jesscruz099@gmail.com>
2025-05-05 15:40:34 -0600

    Limpieza: elimino archivos que no necesitaba

---
+++ Modulo1/Semana2/operadores.py
@@ -0,0 +1,41 @@
+#operadores aritméticos
+# print(2 + 2) # suma
+# print(4 - 2) # resta
+# print(3 * 7) # multip´licacion
+# print(14 / 2) # division
+# print(11 % 4) # modulo
+# print(11 // 4) # division de piso
+# print(2 ** 3) # potenciacion
+
+
+# # operadores en cadenas
+# print("Hola" + " " + "Mundo") # concatenacion
+# print("Hola" * 3) # repeticion
+# print('hola' + 'mundo' * 3) # concatenacion y repeticion
+
+# # operadores de comparacion
+# print('hola' == 'hola') # igual que
+# print('Hola' ¡= 'hola') # distinto que
+# print (3 > 11) # mayor que, menor que
+# print(11 >= 10) # mayor o igual que
+# print(10 <= 10)  # menor o igual
+
+# operadores de existencia
+# print('ola' in 'hola') # de existencia
+# print('ola' not in 'hola') # de no existencia
+
+# operadores booanos
+# print(True or False) # or verifica que se cumpla una de las dos condiciones
+# print(True and False) # and verifica que se cumplan las dos condiciones
+
+# operadores de asignacion
+# x = 1 # operador de asignacion estandar
+# x += 2 # operador de asignacion de suma
+# x *= 3 # operador de asignacion de multiplicacion
+# x %= 4 # operador de asignacion de modulo
+# print(x) 
+
+x = 6 * 5 + 8 / 2 ** 4
+print(x)
+
+ 
---
+++ Modulo1/Semana4/Ejercicio_de_la_semana.py
@@ -0,0 +1,13 @@
+nombre = input('introduce tu nombre: ')
+apellido = input('introduce tu apellido: ')
+edad = int(input('introduce tu edad: '))
+correo = input('introduce el correo electronico: ')
+telefono = input(('introduce tu numero: '))
+
+print('nombre:', nombre)
+print('apellido:', apellido)
+print('edad:', edad)
+print('correo:', correo)
+print('telefono:', telefono)
+
+
